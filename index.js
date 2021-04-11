9b9867b0673cc54029cb3db07a24c2896ccfd9a8
Daniel Amoateng <danielamoateng@iMac-2.local>
2021-04-11 04:33:48 -0400

    Created separate functions to make code look simple and readable

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 
+function calculateNumberandString(){}
+function calculateNullAsZero(){}
+function calculateIgnoreUndefine(){}
+function calculateIgnoreEmptyString(){}
+function calculateNonNumericIgnore(){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function calculate(calculationSteps) {
-  // multiplies two numbers when one is stringified
-
-
-
-  // treats null values as zeroes and includes them in the calculation
-
-
-  // ignores undefined and empty string values and continues operation
-
-
-  // ignores non-numeric values  and continues operation
-
-
-  // returns NaN if the array does not represent a proper operation
-
   switch (calculationSteps) {
     case 'numbers':
+      // multiplies two numbers when one is stringified
+      break
     case 'nullAsZero':
+      // treats null values as zeroes and includes them in the calculation
+      break
     case 'undefinedIngore':
+      // ignores undefined and empty string values and continues operation
+      break
     case 'emptyStringIngore':
+      // ignores non-numeric values  and continues operation
+      break
     case 'nonNumericIgnore':
+      // returns NaN if the array does not represent a proper operation
+      break
     default:
       return NaN
   }

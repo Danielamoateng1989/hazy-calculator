507fb5db44b950d9a292aff01c87659c8c7ac3e2
Daniel Amoateng <danielamoateng@iMac-2.local>
2021-04-11 04:07:41 -0400

    1 test pass when i returned NaN

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function calculate(calculationSteps) {
+  // multiplies two numbers when one is stringified
+
+
+
+  // treats null values as zeroes and includes them in the calculation
+
+
+  // ignores undefined and empty string values and continues operation
+
+
+  // ignores non-numeric values  and continues operation
+
+
+  // returns NaN if the array does not represent a proper operation
+  return NaN
+}
+
+module.exports = calculate

8e11137422f936a9d69b196963a91d0188c4ec92
Daniel Amoateng <danielamoateng@iMac-2.local>
2021-04-11 21:12:31 -0400

    Created a new array converting null values to zero

--- index.js
+++ index.js
@@ -5,25 +5,12 @@ function calculate(calculationSteps) {
     return value === '+' || value === '-' || value === '+'
   }
 
-const operator = calculationSteps.filter((val) => getOperator(val))
+  const operator = calculationSteps.filter((val) => getOperator(val))
 
 
-//Let return NaN if the array does not represent a proper function 
-if (operator.length === 0) return
+  // Let's return NaN if the array does not represent a proper function 
+  if (operator.length === 0) return
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // Let's convert null value to zero
+  let newArray = Array.from(calculationSteps, (item) => item === null ? 0: item)
 }
